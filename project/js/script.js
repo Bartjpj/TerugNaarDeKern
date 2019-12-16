36ae41981a0133e5a7492b5dee464fccc8eefa6e
hansgit1 <katibhans@gmail.com>
2019-12-16 20:15:20 +0100

    namen api ontwikkelt

--- project/js/script.js
+++ project/js/script.js
@@ -5,19 +5,51 @@ function revealspeechbubbleafter() {
 }
 
 function loopskipbtn() {
-       var skipbtn = $('#skipstorybtn');
-       skipbtn.animate({opacity:'1'}, 3000);
-       skipbtn.animate({opacity:'0'}, 1000, loopskipbtn);
-    }
+  var skipbtn = $('#skipstorybtn');
+  skipbtn.animate({
+    opacity: '1'
+  }, 3000);
+  skipbtn.animate({
+    opacity: '0'
+  }, 1000, loopskipbtn);
+}
 
 setTimeout("revealspeechbubbleafter()", 3000);
 setTimeout("loopskipbtn()", 4500);
 
 $.getJSON("js/namen.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+  // console.log(json); // this will show the info it in firebug console
+  var naam = 'Cornelis';
+  var groet = "";
+
+  for (var key in json) {
+    var item = json[key];
+    for (var key2 in item) {
+      var splitchars = item[key2].split(";");
+      naamannen = String(splitchars[3]);
+    }
+
+    if (splitchars[3] == naam){
+      groet = "";
+      groet+= "Hoi " + naam + "! Volgens onze gegevens zijn er " + splitchars[4] + " mensen met jouw naam.";
+      break;
+    }
+    else if (splitchars[1] == naam){
+      groet = "";
+      groet+= "Hoi " + naam + "! Volgens onze gegevens zijn er " + splitchars[2] + " mensen met jouw naam.";
+      break;
+    }
+
+    else{
+      groet = "";
+      groet+='Hoi ' + naam + "!";
+    }
+
+  }
+  console.log(groet);
 });
 
-$('.flex-item').click(function(){
+$('.flex-item').click(function() {
   $('.overlay-dark').css('display', 'block');
   $('.popup-vraag').css('display', 'block');
   $('.popup-weetje').css('display', 'block');
