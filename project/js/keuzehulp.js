2ae23f99e3926ecf468b53a8d5b5acd0e56546e5
xFleur <mels1999@live.nl>
2019-12-20 14:23:18 +0100

    keuzehulp remember question (still bug)

--- project/js/keuzehulp.js
+++ project/js/keuzehulp.js
@@ -5,11 +5,12 @@ var header1 = document.getElementById("vraag-1");
 var btns1 = header1.getElementsByClassName("btnv1");
 for (var i = 0; i < btns1.length; i++) {
   btns1[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) {
-    current[0].className = current[0].className.replace(" active", "");
+  var current1 = document.getElementsByClassName("active");
+  if (current1.length > 0) {
+    current1[0].className = current1[0].className.replace(" active", "");
   }
   this.className += " active";
+  var v1state = True;
   });
 }
 
@@ -18,9 +19,9 @@ var header2 = document.getElementById("vraag-2");
 var btns2 = header2.getElementsByClassName("btnv2");
 for (var i = 0; i < btns2.length; i++) {
   btns2[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) {
-    current[0].className = current[0].className.replace(" active", "");
+  var current2 = document.getElementsByClassName("active");
+  if (current2.length > 0) {
+    current2[0].className = current2[0].className.replace(" active", "");
   }
   this.className += " active";
   });
