48d4a2f700a618f15e77a463eb0c99d79ae2b97d
hansgit1 <katibhans@gmail.com>
2019-12-25 15:32:23 +0100

    Tooltip toegevoegd voornamen api
    Een tooltip wanneer die gegevens vind over de naam. Om aan te tonen waar de gegevens van de voornamen afkomstig is.

--- project/js/namenapi.js
+++ project/js/namenapi.js
@@ -1,5 +1,17 @@
 var groet = "";
 
+var div = document.createElement('div');
+div.textContent = "gegevens";
+div.setAttribute('class', 'tooltip');
+var span = document.createElement('span');
+span.textContent = "Onze data is afkomstig van de De Nederlandse Voornamenbank van het Meertens Instituut KNAW. Zij publiceerden een bestand (uit 2010) met meer dan 500 eerste mannelijke en vrouwelijke eerste voornamen (met de Nederlandse nationaliteit en wonend in Nederland). Voor meer info zie:  www.meertens.knaw.nl/nvb";
+span.setAttribute('class', 'tooltiptext');
+div.appendChild(span);
+
+var gegevens = document.getElementById('gegevens');
+gegevens.appendChild(div);
+$('#gegevens').css('display', 'none');
+
 $("#btnNaam").click(function(){
   var naam = document.getElementById('naam').value;
   naam = naam.charAt(0).toUpperCase() + naam.slice(1);
@@ -15,13 +27,13 @@ $("#btnNaam").click(function(){
 
       if (splitchars[3] == naam){
         groet = "";
-        groet+= "Hey " + naam + ", leuk je te ontmoeten. Volgens onze gegevens zijn er " + splitchars[4] + " mensen met jouw naam.";
+        groet+= "Hey " + naam + ", leuk je te ontmoeten. Volgens onze " + gegevens.innerHTML + " zijn er " + splitchars[4] + " mensen met jouw naam.";
         document.getElementById('naamgroet').innerHTML = groet;
         break;
       }
       else if (splitchars[1] == naam){
         groet = "";
-        groet+= "Hey " + naam + ", leuk je te ontmoeten. Volgens onze gegevens zijn er " + splitchars[2] + " mensen met jouw naam.";
+        groet+= "Hey " + naam + ", leuk je te ontmoeten. Volgens onze" + gegevens.innerHTML + "zijn er " + splitchars[2] + " mensen met jouw naam.";
         document.getElementById('naamgroet').innerHTML = groet;
         break;
       }
