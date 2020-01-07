74867907e072f9243da59e76fd5744cb8173cccc
xFleur <mels1999@live.nl>
2020-01-07 16:17:04 +0100

    keuze styling

--- project/js/keuzehulp.js
+++ project/js/keuzehulp.js
@@ -38,6 +38,146 @@ var val;
 
 // if($('input[value="huisarts"]').not(this).prop('checked', false));
 // });
+// show vraag
+$('#item-1').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-1').css('display', 'block');
+  $('#weetje-1').css('display', 'block');
+});
+
+$('#item-2').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-2').css('display', 'block');
+  $('#weetje-2').css('display', 'block');
+});
+$('#item-3').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-3').css('display', 'block');
+  $('#weetje-3').css('display', 'block');
+});
+$('#item-4').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-4').css('display', 'block');
+  $('#weetje-4').css('display', 'block');
+});
+$('#item-5').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-5').css('display', 'block');
+  $('#weetje-5').css('display', 'block');
+});
+$('#item-6').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-6').css('display', 'block');
+  $('#weetje-6').css('display', 'block');
+});
+$('#item-7').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-7').css('display', 'block');
+  $('#weetje-7').css('display', 'block');
+});
+$('#item-8').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-8').css('display', 'block');
+  $('#weetje-8').css('display', 'block');
+});
+$('#item-9').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-9').css('display', 'block');
+  $('#weetje-9').css('display', 'block');
+});
+$('#item-10').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-10').css('display', 'block');
+  $('#weetje-10').css('display', 'block');
+});
+$('#item-11').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-11').css('display', 'block');
+  $('#weetje-11').css('display', 'block');
+});
+$('#item-12').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-12').css('display', 'block');
+  $('#weetje-12').css('display', 'block');
+});
+$('#item-13').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-13').css('display', 'block');
+  $('#weetje-13').css('display', 'block');
+});
+$('#item-14').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-14').css('display', 'block');
+  $('#weetje-14').css('display', 'block');
+});
+$('#item-15').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-15').css('display', 'block');
+  $('#weetje-15').css('display', 'block');
+});
+$('#item-16').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-16').css('display', 'block');
+  $('#weetje-16').css('display', 'block');
+});
+$('#item-17').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-17').css('display', 'block');
+  $('#weetje-17').css('display', 'block');
+});
+$('#item-18').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-18').css('display', 'block');
+  $('#weetje-18').css('display', 'block');
+});
+$('#item-19').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-19').css('display', 'block');
+  $('#weetje-19').css('display', 'block');
+});
+$('#item-20').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-20').css('display', 'block');
+  $('#weetje-20').css('display', 'block');
+});
+$('#item-21').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-21').css('display', 'block');
+  $('#weetje-21').css('display', 'block');
+});
+$('#item-22').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-22').css('display', 'block');
+  $('#weetje-22').css('display', 'block');
+});
+$('#item-23').click(function() {
+  $('.overlay-dark').css('display', 'block');
+  $('#vraag-23').css('display', 'block');
+  $('#weetje-23').css('display', 'block');
+});
+
+
+// overlay vraag + wegklikken
+$('.overlay-dark').click(function() {
+  $('.popup-vraag').css('display', 'none');
+  $('.overlay-dark').css('display', 'none');
+  $('.popup-weetje').css('display', 'none');
+});
+
+$('.popup-vraag').click(function() {
+  $('.popup-vraag').css('z-index', '101');
+  $('.popup-weetje').css('z-index', '100');
+});
+$('.popup-weetje').click(function() {
+  $('.popup-vraag').css('z-index', '100');
+  $('.popup-weetje').css('z-index', '101');
+});
+
+$('.button').click(function() {
+  $('.popup-vraag').css('display', 'none');
+  $('.overlay-dark').css('display', 'none');
+  $('.popup-weetje').css('display', 'none');
+});
 
 $('input[type="checkbox"]').on('change', function() {
   $('input[name="' + this.name + '"]').not(this).prop('checked', false);
@@ -159,6 +299,25 @@ $('input[type="checkbox"]').on('change', function() {
       $('.chrom23').css({ fill: "#E94C4C" });
     } else {
       $('.chrom23').css({ fill: "#fff" });
+    }
+
+    if(  $('input[name="group1[]"]').is(':checked') | $('input[name="group2[]"]').is(':checked') | $('input[name="group3[]"]').is(':checked') | $('input[name="group4[]"]').is(':checked') 
+    | $('input[name="group5[]"]').is(':checked') | $('input[name="group6[]"]').is(':checked') | $('input[name="group7[]"]').is(':checked') | $('input[name="group8[]"]').is(':checked') 
+    | $('input[name="group9[]"]').is(':checked') | $('input[name="group10[]"]').is(':checked') | $('input[name="group11[]"]').is(':checked') | $('input[name="group12[]"]').is(':checked') 
+    | $('input[name="group13[]"]').is(':checked') | $('input[name="group14[]"]').is(':checked') | $('input[name="group15[]"]').is(':checked') | $('input[name="group16[]"]').is(':checked') 
+    | $('input[name="group17[]"]').is(':checked') | $('input[name="group18[]"]').is(':checked') | $('input[name="group19[]"]').is(':checked') | $('input[name="group20[]"]').is(':checked') 
+    | $('input[name="group21[]"]').is(':checked') | $('input[name="group22[]"]').is(':checked') | $('input[name="group23[]"]').is(':checked')
+    ) {
+      $(".redish-naarres").css("background-color","rgba(255, 82, 82, 0.21)");
+    }
+    if(  $('input[name="group1[]"]').is(':checked') && $('input[name="group2[]"]').is(':checked') && $('input[name="group3[]"]').is(':checked') && $('input[name="group4[]"]').is(':checked') 
+    && $('input[name="group5[]"]').is(':checked') && $('input[name="group6[]"]').is(':checked') && $('input[name="group7[]"]').is(':checked') && $('input[name="group8[]"]').is(':checked') 
+    && $('input[name="group9[]"]').is(':checked') && $('input[name="group10[]"]').is(':checked') && $('input[name="group11[]"]').is(':checked') && $('input[name="group12[]"]').is(':checked') 
+    && $('input[name="group13[]"]').is(':checked') && $('input[name="group14[]"]').is(':checked') && $('input[name="group15[]"]').is(':checked') && $('input[name="group16[]"]').is(':checked') 
+    && $('input[name="group17[]"]').is(':checked') && $('input[name="group18[]"]').is(':checked') && $('input[name="group19[]"]').is(':checked') && $('input[name="group20[]"]').is(':checked') 
+    && $('input[name="group21[]"]').is(':checked') && $('input[name="group22[]"]').is(':checked') && $('input[name="group23[]"]').is(':checked')
+    ) {
+      $(".redish-naarres").css("background-color","rgba(255, 82, 82, 0.91)");
     }
   
 });
